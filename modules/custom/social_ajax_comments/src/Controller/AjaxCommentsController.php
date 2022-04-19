51647ba9c16cd785b38e1109053abeec0b226407
Oleksandr Maksymiuk <alexander.maximiuk@onbox.app>
2022-04-19 13:31:23 +0300

    Issue #3275943 by alex.ksis: Reset cache of the commented entity.

--- modules/custom/social_ajax_comments/src/Controller/AjaxCommentsController.php
+++ modules/custom/social_ajax_comments/src/Controller/AjaxCommentsController.php
@@ -181,6 +181,13 @@ class AjaxCommentsController extends ContribController {
       // AjaxCommentsForm::save() during the previous HTTP request.
       $cid = $this->tempStore->getCid();
 
+      // Reset cache of the commented entity.
+      if ($cid) {
+        $this->entityTypeManager()
+          ->getStorage($entity->getEntityTypeId())
+          ->resetCache([$entity->id()]);
+      }
+
       // Try to insert the message above the new comment.
       if (
         !empty($cid) &&
